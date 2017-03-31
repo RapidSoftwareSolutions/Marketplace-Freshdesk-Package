2dd8136f151f5a9d48f64697a3348766d836d4a5
ustal <quit.ipad2@gmail.com>
2017-03-31 19:32:55 +0300

    fix updateForumCategory

--- src/routes/updateForumCategory.php
+++ src/routes/updateForumCategory.php
@@ -7,7 +7,7 @@ $app->post('/api/Freshdesk/updateForumCategory', function ($request, $response) 
 
     $settings = $this->settings;
     $checkRequest = $this->validation;
-    $validateRes = $checkRequest->validate($request, ['apiKey', 'domain', 'categoryId', 'name']);
+    $validateRes = $checkRequest->validate($request, ['apiKey', 'domain', 'categoryId']);
     if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
         return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
     } else {
@@ -19,54 +19,65 @@ $app->post('/api/Freshdesk/updateForumCategory', function ($request, $response) 
     $headers['Authorization'] = "Basic " . base64_encode($postData['args']['apiKey']);
     $headers['Content-Type'] = 'application/json';
 
-    $json['name'] = $postData['args']['name'];
     if (isset($postData['args']['description']) && strlen($postData['args']['description']) > 0) {
         $json['description'] = $postData['args']['description'];
     }
+    if (isset($postData['args']['name']) && strlen($postData['args']['name']) > 0) {
+        $json['name'] = $postData['args']['name'];
+    }
 
-    try {
-        /** @var GuzzleHttp\Client $client */
-        $client = $this->httpClient;
-        $vendorResponse = $client->put($url, [
-            'headers' => $headers,
-            'json' => $json
-        ]);
-        $vendorResponseBody = $vendorResponse->getBody()->getContents();
-        if ($vendorResponse->getStatusCode() == 200) {
-            $result['callback'] = 'success';
-            $result['contextWrites']['to'] = [
-                "result" => json_decode($vendorResponse->getBody()),
-                "info" => [
-                    "X-Freshdesk-API-Version" => $vendorResponse->getHeader("X-Freshdesk-API-Version")[0],
-                    "X-RateLimit-Remaining" => $vendorResponse->getHeader("X-RateLimit-Remaining")[0],
-                    "X-RateLimit-Total" => $vendorResponse->getHeader("X-RateLimit-Total")[0],
-                    "X-RateLimit-Used-CurrentRequest" => $vendorResponse->getHeader("X-RateLimit-Used-CurrentRequest")[0]
-                ]
-            ];
-        }
-        else {
+    if (!empty($json)) {
+        try {
+            /** @var GuzzleHttp\Client $client */
+            $client = $this->httpClient;
+            $vendorResponse = $client->put($url, [
+                'headers' => $headers,
+                'json' => $json
+            ]);
+            $vendorResponseBody = $vendorResponse->getBody()->getContents();
+            if ($vendorResponse->getStatusCode() == 200) {
+                $result['callback'] = 'success';
+                $result['contextWrites']['to'] = [
+                    "result" => json_decode($vendorResponse->getBody()),
+                    "info" => [
+                        "X-Freshdesk-API-Version" => $vendorResponse->getHeader("X-Freshdesk-API-Version")[0],
+                        "X-RateLimit-Remaining" => $vendorResponse->getHeader("X-RateLimit-Remaining")[0],
+                        "X-RateLimit-Total" => $vendorResponse->getHeader("X-RateLimit-Total")[0],
+                        "X-RateLimit-Used-CurrentRequest" => $vendorResponse->getHeader("X-RateLimit-Used-CurrentRequest")[0]
+                    ]
+                ];
+            } else {
+                $result['callback'] = 'error';
+                $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+                $result['contextWrites']['to']['status_msg'] = is_array($vendorResponseBody) ? $vendorResponseBody : json_decode($vendorResponseBody);
+            }
+        } catch (\GuzzleHttp\Exception\ServerException $exception) {
             $result['callback'] = 'error';
             $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-            $result['contextWrites']['to']['status_msg'] = is_array($vendorResponseBody) ? $vendorResponseBody : json_decode($vendorResponseBody);
-        }
-    } catch (\GuzzleHttp\Exception\ServerException $exception) {
-        $result['callback'] = 'error';
-        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-        $result['contextWrites']['to']['status_msg'] = json_decode($exception->getResponse()->getBody());
+            $result['contextWrites']['to']['status_msg'] = json_decode($exception->getResponse()->getBody());
 
-    } catch (\GuzzleHttp\Exception\ClientException $exception) {
+        } catch (\GuzzleHttp\Exception\ClientException $exception) {
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg']['result'] = json_decode($exception->getResponse()->getBody()->getContents(), true);
+            $result['contextWrites']['to']['status_msg']['info'] = [
+                "X-Freshdesk-API-Version" => $exception->getResponse()->getHeader("X-Freshdesk-API-Version")[0],
+                "X-RateLimit-Remaining" => $exception->getResponse()->getHeader("X-RateLimit-Remaining")[0],
+                "X-RateLimit-Total" => $exception->getResponse()->getHeader("X-RateLimit-Total")[0],
+                "X-RateLimit-Used-CurrentRequest" => $exception->getResponse()->getHeader("X-RateLimit-Used-CurrentRequest")[0]
+            ];
+            if ($exception->getCode() == 429) {
+                $result['contextWrites']['to']['status_msg']['info']['Retry-After'] = $exception->getResponse()->getHeader("Retry-After");
+            }
+        }
+    }
+    else {
         $result['callback'] = 'error';
         $result['contextWrites']['to']['status_code'] = 'API_ERROR';
-        $result['contextWrites']['to']['status_msg']['result'] = json_decode($exception->getResponse()->getBody()->getContents(), true);
-        $result['contextWrites']['to']['status_msg']['info'] = [
-            "X-Freshdesk-API-Version" => $exception->getResponse()->getHeader("X-Freshdesk-API-Version")[0],
-            "X-RateLimit-Remaining" => $exception->getResponse()->getHeader("X-RateLimit-Remaining")[0],
-            "X-RateLimit-Total" => $exception->getResponse()->getHeader("X-RateLimit-Total")[0],
-            "X-RateLimit-Used-CurrentRequest" => $exception->getResponse()->getHeader("X-RateLimit-Used-CurrentRequest")[0]
+        $result['contextWrites']['to']['status_msg']['result'] = [
+            "code" => "missing_params",
+            "message" => "Your update call does not have any parameter. Atleast one parameter is required."
         ];
-        if ($exception->getCode() == 429) {
-            $result['contextWrites']['to']['status_msg']['info']['Retry-After'] = $exception->getResponse()->getHeader("Retry-After");
-        }
     }
 
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);

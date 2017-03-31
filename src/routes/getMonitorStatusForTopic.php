6fc489d5ebfbdc43e10180f70089b83dc063f450
ustal <quit.ipad2@gmail.com>
2017-03-31 19:22:50 +0300

    fix getMonitorStatusForTopic

--- src/routes/getMonitorStatusForTopic.php
+++ src/routes/getMonitorStatusForTopic.php
@@ -57,6 +57,7 @@ $app->post('/api/Freshdesk/getMonitorStatusForTopic', function ($request, $respo
 
     } catch (\GuzzleHttp\Exception\ClientException $exception) {
         if ($exception->getCode() == 404) {
+            $result['callback'] = 'error';
             $result['contextWrites']['to'] = [
                 "result" => false,
                 "info" => [
